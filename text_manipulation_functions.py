0dc97a22848441181427e47f1188d5b04abc031f
poc1673Laptop <petercaya@gmail.com>
2019-11-23 18:51:28 -0700

    Addition of scripts containing placeholders for functions to calculate the proficiency with a new word and to perform the manipulations for strings of text. git push -u origin master

---
+++ text_manipulation_functions.py
@@ -0,0 +1,15 @@
+# Peter Caya
+# Function to take a corpus of words and output a list of sentences.
+
+def to_sentences(corpus):
+    
+
+
+
+# Function to tell whether an n-gram is in a string of text.
+
+def detect_ngram(text,ngram):
+
+
+
+ 
---
+++ text_scoring_functions.py
@@ -0,0 +1,2 @@
+# Peter Caya
+
