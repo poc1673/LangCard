25f5a66a848b345d94b3613dd382e7c2f160372f
poc1673Laptop <petercaya@gmail.com>
2019-11-30 14:48:00 -0700

    Addition #1: Added function to filter for only sentences and longer texts. Addition #2 is a function to clean the strings for run-on text after the period.

--- text_manipulation_functions.py
+++ text_manipulation_functions.py
@@ -1,12 +1,25 @@
 # Peter Caya
 # Function to take a corpus of words and output a list of sentences.
 
-def to_sentences(corpus):
-    
+import pandas as pd
+import re
+#os.chdir("Dropbox/Projects/Adaptive Flash Cards/")
+#a = pd.read_csv("Data/2019-11-24 Testing Data.csv",encoding = "ISO-8859-1",squeeze = True).x
+
+def detect_periods(string):
+    return(string.find(".")>=0)
+
+# Function to apply filtering criteria as the first step of the data cleaning process. It omits "junk" rows which contain only a few characters
+# and selects for cases where the string ends with a period.
+def filter_corpus(string):
+     inds = string.apply(lambda x: (detect_periods(x)&(len(x)>5 ) ))
+     return(string[inds])
+     
+def rm_end_white(x):
+    return( re.sub(string= x,repl = ".",pattern = "\..*$"))
 
 
 
-# Function to tell whether an n-gram is in a string of text.
 
 def detect_ngram(text,ngram):
 
